1548cd4974be441dd850bd2713495961880ef285
Sujan Poudel <sujanp@skybasestudios.com.np>
2022-12-19 19:36:19 +0545

    feat : blog crud views

--- src/views/Admin/Blogs/partials/BlogForm.vue
+++ src/views/Admin/Blogs/partials/BlogForm.vue
@@ -1,93 +1,182 @@
 <template>
-  <div>
-    <Form>
-      <base-row class="">
-        <input-text
-          label="Title"
-          :rules="{ required: true }"
-          name="title"
-          :vmodel.sync="form.title"
-        />
-        <input-select
-          label="Category"
-          :rules="{ required: true }"
-          name="category"
-          :vmodel.sync="form.category"
-          :options="categoryOptions"
-        />
-      </base-row>
-      <base-row class="">
-        <input-radio
-          label="Status"
-          :rules="{ required: false }"
-          name="status"
-          :vmodel.sync="form.status"
-          :options="statusOptions"
-        />
-      </base-row>
-      <base-row>
-        <input-text-area
-          label="Content"
-          name="content"
-          :rules="{ required: true }"
-          :vmodel.sync="form.content"
-          field-class="w-12 mb-18"
-        />
+  <base-modal title="Add blog" @on-close="closeModal" :show="show">
+    <Form @submit="onSubmit" ref="blogForm" :key="formRenderKey">
+      <base-row v-for="(item, i) in formContainer" :key="i">
+        <component
+          :is="item.componentValue"
+          v-bind="{
+            ...item.properties,
+          }"
+          v-model="form[item.formKey]"
+        ></component>
       </base-row>
       <base-row>
         <div class="w-12">
           <div class="flx flx-end mt-15">
             <base-button class="mr-9">Add Blog</base-button>
-            <base-button class="outline">Close</base-button>
+            <base-button class="outline" @click.stop="closeModal"
+              >Close</base-button
+            >
           </div>
         </div>
       </base-row>
     </Form>
-  </div>
+  </base-modal>
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
+import InputCkEditor from "../../../../components/form/InputCkEditor.vue";
+import { reactive, ref, computed, watch } from "vue";
 import { Form } from "vee-validate";
 import InputText from "../../../../components/form/InputText.vue";
 import InputSelect from "../../../../components/form/InputSelect.vue";
 import InputRadio from "../../../../components/form/InputRadio.vue";
-import InputTextArea from "../../../../components/form/InputTextArea.vue";
+import useBlog from "@/composables/useBlog";
+import { useForm, useField } from "vee-validate";
+/**Interfaces */
+interface categoryOption {
+  id: string | number;
+  name: string;
+}
+interface Props {
+  categoryOptions: categoryOption[];
+  show: boolean;
+}
 
-let form = reactive({
+/**Props */
+const props = defineProps<Props>();
+
+// Emits
+const emit = defineEmits<{
+  (e: "onClose"): void;
+  (e: "onSuccess"): void;
+}>();
+
+/**Data */
+const formFields = {
   title: "",
   category: "",
   status: "",
   content: "",
+};
+
+let form = reactive({ ...formFields });
+const blogForm = ref(null);
+const formRenderKey = ref(1);
+
+/**Composables */
+const { createBlog } = useBlog();
+
+const { resetForm } = useForm();
+const { resetField } = useField("field", (value) => !!value);
+
+/**Computed Properties */
+const formContainer = computed(() => {
+  return [
+    {
+      componentValue: InputText,
+      properties: {
+        label: "Title",
+        name: "title",
+        rules: { required: true },
+        fieldClass: "w-12 mb-18",
+        modelValue: form.title,
+      },
+      formKey: "title",
+    },
+    {
+      componentValue: InputSelect,
+      properties: {
+        label: "Category",
+        name: "category",
+        rules: { required: true },
+        modelValue: form.category,
+        options: props.categoryOptions.length
+          ? props.categoryOptions.map(({ name }) => {
+              return {
+                value: name,
+                name: name,
+              };
+            })
+          : [],
+        fieldClass: "w-12 mb-18",
+      },
+      formKey: "category",
+    },
+    {
+      componentValue: InputRadio,
+      properties: {
+        label: "Status",
+        name: "status",
+        rules: { required: true },
+        modelValue: form.status,
+        options: [
+          { name: "Active", value: "active" },
+          { name: "Inactive", value: "inactive" },
+        ],
+        fieldClass: "w-12 mb-18",
+      },
+      formKey: "status",
+    },
+    {
+      componentValue: InputCkEditor,
+      properties: {
+        label: "Content",
+        name: "content",
+        rules: { required: true },
+        modelValue: form.content,
+        fieldClass: "w-12 mb-18",
+      },
+      formKey: "content",
+    },
+  ];
 });
 
-let categoryOptions = reactive([
-  {
-    name: "Politics",
-    value: "politics",
-  },
-  {
-    name: "Fashion",
-    value: "fashion",
-  },
-  {
-    name: "Culture",
-    value: "culture",
-  },
-  {
-    name: "Travel & Blog",
-    value: "travel&blog",
-  },
-  {
-    name: "Movies & Entertainment",
-    value: "movies&entertainment",
-  },
-]);
+/**Methods */
+const onSubmit = async () => {
+  console.log("submitting", form);
+  createBlog(form);
+  emit("onSuccess");
+  closeModal();
+};
 
-let statusOptions = [
-  { name: "Active", value: "active" },
-  { name: "Inactive", value: "inactive" },
-];
+const resetFormValue = () => {
+  console.log("resetting");
+  Object.assign(form, formFields);
+};
+
+const closeModal = async () => {
+  resetFormValue();
+  const r = resetForm({
+    values: {
+      title: "",
+      status: "",
+      category: "",
+      content: "",
+    },
+  });
+  resetField();
+  formRenderKey.value++;
+  console.log("here closing", r);
+  emit("onClose");
+};
+
+watch(
+  () => props.show,
+  (newVal, oldVal) => {
+    console.log("watching", newVal, oldVal);
+    if (newVal) {
+      resetForm({
+        values: {
+          title: "",
+          status: "",
+          category: "",
+          content: "",
+        },
+      });
+    }
+  }
+);
 </script>
 
 <style>
--- src/views/Admin/Blogs/partials/BlogTable.vue
+++ src/views/Admin/Blogs/partials/BlogTable.vue
@@ -1,21 +1,29 @@
 <template>
-  <tr>
-    <td>1</td>
-    <td>Laravel</td>
-    <td>Programming</td>
-    <td>Active</td>
+  <tr v-for="(row, i) in rows" :key="i">
+    <td>{{ row.id }}</td>
+    <td>{{ row.title }}</td>
+    <td>{{ row.category }}</td>
+    <td>{{ row.status ? "Active" : "Inactive" }}</td>
     <td>
       <CrudActions></CrudActions>
     </td>
   </tr>
-  
+  <template> </template>
 </template>
 
 <script setup lang="ts">
-import CrudActions from '../../../../components/CrudActions.vue'
+import CrudActions from "../../../../components/CrudActions.vue";
+interface row {
+  id: "";
+  title: "";
+  status: "";
+  category: "";
+}
 
+interface Props {
+  rows?: row[];
+}
+defineProps<Props>();
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
