3ac131e96db39e9cdd05c1ad87a2cb9d588cb91d
Sujan Poudel <sujanp@skybasestudios.com.np>
2022-12-29 11:02:06 +0545

    fix & improve : css styles

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
-import { createApp, useAttrs } from "vue";
+import { createApp} from "vue";
 import App from "./App.vue";
 import "./assets/main.scss";
 
 // Global Components Regiter
-import { registerComponents } from "./utils/global-components"
+import { registerComponents } from "./utils/global-components";
+
+/**
+ * additional third party libraries and plugins
+ */
+import LoadingSpinner from "./plugins/spinner";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 //Use Utils
-import './utils/validator.ts';
-/** Router*/
+import "./utils/validator.ts";
 
-import router from './router'
+/** Router*/
+import router from "./router";
 
 const app = createApp(App);
 registerComponents(app);
+app.use(LoadingSpinner);
+app.use(Toast);
 app.use(router);
+
 app.mount("#app");
