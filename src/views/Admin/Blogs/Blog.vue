abe0e8500d564314986b87a2a70f31947da36597
Sujan Poudel <sujanp@skybasestudios.com.np>
2023-01-06 08:50:08 +0545

    chore : add pagination component in blog

--- src/views/Admin/Blogs/Blog.vue
+++ src/views/Admin/Blogs/Blog.vue
@@ -9,26 +9,35 @@
       </BaseButton>
     </base-row>
 
-    <base-row class="shadow-normal p-8 round-10">
+    <div class="shadow-normal p-8 round-10">
       <BaseTable :headers="['#Id', 'Name', 'Category', 'Status', 'Action']">
         <BlogTable
-          :rows="blogRows"
+          :rows="rows[currentPage - 1]"
           @handle-on-edit="openModal"
           @handle-on-delete="(id:string)=>deleteRow(id)"
         />
       </BaseTable>
+    </div>
+    <pagination-links
+      :current-page="currentPage"
+      :total-pages="rows.length"
+      @page-change="setCurrentPage"
+    />
+
+    <template #modals>
       <blog-form
         v-model:show="show"
         :category-options="categoryRows"
         @on-success="getBlogs"
         :row="row"
       ></blog-form>
-    </base-row>
+    </template>
   </dashboard-layout>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import PaginationLinks from "../../../components/ui/PaginationLinks.vue";
+import { ref, watch, onMounted } from "vue";
 import useBlog from "@/composables/useBlog";
 import BlogForm from "./partials/BlogForm.vue";
 import BlogTable from "./partials/BlogTable.vue";
@@ -36,13 +45,17 @@ import BaseTable from "../../../components/base/BaseTable.vue";
 import useCategory from "@/composables/useCategories";
 import type { BlogRow } from "@/types";
 import { showToast, useMessage } from "@/composables/useToast";
+import usePagination from "@/composables/usePagination";
 import { SWR } from "@/common/constant";
 
 /** reactiuve & non reactive data*/
+const currentPage = ref<number>(1);
 const show = ref<boolean>(false);
+const rows = ref<any[]>([]);
 const row = ref<BlogRow>(null);
 const { blogRows, getBlogs, deleteBlog } = useBlog();
 const { categoryRows, getCategories } = useCategory();
+const { chunkItems } = usePagination();
 /** computed properties */
 /** functions */
 const openModal = (rowData: BlogRow = null) => {
@@ -54,12 +67,21 @@ const deleteRow = async (id: string) => {
   let res = await deleteBlog(id);
   if (res) {
     showToast(useMessage("Blog", "delete"), "success");
+    setCurrentPage(1);
     getBlogs();
   } else {
     showToast(SWR, "error");
   }
 };
+const setCurrentPage = (pageNo: number) => {
+  currentPage.value = pageNo;
+};
 /** Watchers */
+watch(blogRows, (newRow) => {
+  if (newRow && newRow.length) {
+    rows.value = chunkItems(newRow as []);
+  }
+});
 /** lifecycle hooks */
 onMounted(() => {
   getBlogs();
