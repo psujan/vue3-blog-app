ee39fdba54a72c8702a4f08c23e4c6f7e6cfc672
Sujan Poudel <sujanp@skybasestudios.com.np>
2023-01-06 08:47:06 +0545

    chore : add helper function to get chunked array

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -1,1 +1,9 @@
-export default {};
+export const getChunkedArray = (items: [], size: number = 10) => {
+  let chunkedArray = [];
+  for (let i = 0; i < items.length; i += size) {
+    let tempArray = [];
+    tempArray = items.slice(i, i + size);
+    chunkedArray.push(tempArray);
+  }
+  return chunkedArray;
+};
