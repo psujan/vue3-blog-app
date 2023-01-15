54efabca504ae25f57e9d21a5008599fb0c91415
Sujan Poudel <sujanp@skybasestudios.com.np>
2023-01-15 19:27:52 +0545

    chore : add constant recent blog count

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -2,3 +2,4 @@ export const SWR = "Something Went Wrong";
 export const createMessage = (title: string) => `${title} created successfully`;
 export const updateMessage = (title: string) => `${title} updated successfully`;
 export const deleteMessage = (title: string) => `${title} deleted successfully`;
+export const recentBlogCount = 5;
